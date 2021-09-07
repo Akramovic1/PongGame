4406ea316e751ee3ffd9a6e1b1090a8f432f9dfa
Akramovic1 <eng.akramovic@gmail.com>
2021-09-08 01:02:31 +0200

    Create 2 paddles

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ screen.setup(800, 500)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
-paddle = Paddle()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 screen.listen()
-screen.onkey(paddle.goUP, "Up")
-screen.onkey(paddle.goDOWN, "Down")
+screen.onkey(r_paddle.goUP, "Up")
+screen.onkey(r_paddle.goDOWN, "Down")
 game_is_on = True
 
 while game_is_on:
--- paddle.py
+++ paddle.py
@@ -3,16 +3,16 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, xy):
         super().__init__()
-        self.createPaddle()
+        self.createPaddle(xy)
 
-    def createPaddle(self):
+    def createPaddle(self,xy):
         self.shape("square")
         self.color("white")
         self.penup()
         self.turtlesize(stretch_wid=5, stretch_len=1)
-        self.goto(350, 0)
+        self.goto(xy)
 
     def goUP(self):
         self.goto(self.xcor(), (self.ycor() + 20))
