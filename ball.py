180ce54ae879ab151514d582e370a150bc2f9f89
Akramovic1 <eng.akramovic@gmail.com>
2021-09-08 16:08:11 +0200

    collisions added

--- ball.py
+++ ball.py
@@ -8,6 +8,14 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        self.goto(self.xcor() + 10, self.ycor() + 10)
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
