7e1f5db2033dd87bbdb74cec1109ee98adf5ff6c
Akramovic1 <eng.akramovic@gmail.com>
2021-09-08 17:38:42 +0200

    scoreboard added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
 from time import sleep
+from scoreboard import Scoreboard
 
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(r_paddle.goUP, "Up")
 screen.onkey(r_paddle.goDOWN, "Down")
@@ -20,7 +22,8 @@ screen.onkey(l_paddle.goDOWN, "s")
 game_is_on = True
 
 while game_is_on:
-    sleep(0.08)
+
+    sleep(0.1)
     screen.update()
     ball.move()
 
@@ -30,5 +33,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 \
             or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.resetPosition()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.resetPosition()
+        scoreboard.r_point()
 
 screen.exitonclick()
